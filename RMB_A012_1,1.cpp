8366d762b949aa075446629756bcba769456a58c
BrayanArM98 <141684279+BrayanArM98@users.noreply.github.com>
2023-11-09 20:49:59 -0800

    Add files via upload

---
+++ RMB_A012_1,1.cpp
@@ -0,0 +1,13 @@
+// Brayan Arturo Rocha Meneses  Matricula: 371049
+// Fecha 9/11/2023  9/11/2023
+//Archvos de texto
+// RMB_A012_01,1_432
+
+
+#include"tren.h"
+
+int main()
+{
+    agregar();
+    return 0;
+}
---
+++ RMB_A012_1.cpp
@@ -0,0 +1,14 @@
+// Brayan Arturo Rocha Meneses  Matricula: 371049
+// Fecha 9/11/2023  9/11/2023
+//ARCHIVOS DE TEXTO
+// RMB_A012_01_432
+
+
+
+#include "tren.h"
+
+int main()
+{
+    recolector_datos();//
+    return 0;
+}  

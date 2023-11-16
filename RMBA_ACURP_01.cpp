7a9bb78aa94989e89c0163940bb675f964feca50
BrayanArM98 <141684279+BrayanArM98@users.noreply.github.com>
2023-11-15 17:21:45 -0800

    Add files via upload

---
+++ RMBA_ACURP_01.cpp
@@ -0,0 +1,13 @@
+// Brayan Arturo Rocha Meneses  Matricula: 371049
+// Fecha 9/11/2023  9/11/2023
+//ARCHIVOS DE TEXTO/CURP
+// RMB_A012_01_432
+
+
+#include "tren.h"
+
+int main()
+{
+    recolector_datos();//
+    return 0;
+}  
